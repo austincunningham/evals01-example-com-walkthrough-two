5574e4397090454d19412351413000708e506482
Austin Cunningham <aucunnin@redhat.com>
2018-11-08 12:58:50 +0000

    Added north pole

--- src/main/java/com/redhat/fuse/boosters/rest/http/ArrivalsServiceImpl.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/ArrivalsServiceImpl.java
@@ -15,7 +15,8 @@ public class ArrivalsServiceImpl implements ArrivalsService {
             new Arrival("DUB", 1538050030000l),
             new Arrival("JFK", 1538051000000l),
             new Arrival("RDU", 1538052030000l),
-            new Arrival("ATL", 1538053030000l)
+            new Arrival("ATL", 1538053030000l),
+            new Arrival("NORTH-POLE", 1538053030000l)
         );
     }
 
